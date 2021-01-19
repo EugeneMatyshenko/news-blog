d4a2938967a5257ad6e61c97ae99b39e68c3e46b
cl <cl@MacBook-Pro-cl.local>
2021-01-19 18:32:39 +0300

    Added first version ArticleController

---
+++ app/controllers/ArticleController.rb
@@ -0,0 +1,48 @@
+class ArticleController < ApplicationController
+  def index
+    @article = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :user, :body)
+  end
+
+end
